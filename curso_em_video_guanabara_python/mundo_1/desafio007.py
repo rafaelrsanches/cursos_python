e2d40f07135499e81982dd08cf6fe255ab9df30a
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-08-09 22:17:50 -0300

    Exercício 007 resolvido

---
+++ curso_em_video_guanabara_python/mundo_1/desafio007.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+
+print('A média entre as notas {:.1f} e {:.1f} é igual a {:.1f}'.format(n1, n2, (n1+n2)/2))
