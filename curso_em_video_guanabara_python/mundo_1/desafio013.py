4eb6fd1811a6e4476602b45c80949bad6ffcd954
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-08-12 12:06:49 -0300

    Exercício 013 resolvido

---
+++ curso_em_video_guanabara_python/mundo_1/desafio013.py
@@ -0,0 +1,3 @@
+salário = float(input('Qual é o salário do funcionário? R$'))
+
+print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passar a receber R${:.2f}'.format(salário, salário*1.15))
