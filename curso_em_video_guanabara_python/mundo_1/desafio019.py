0737a5afc8acbf908ab5fc67e6d6405b0b9aa1e2
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-08-14 23:20:58 -0300

    Exercício 019 resolvido

---
+++ curso_em_video_guanabara_python/mundo_1/desafio019.py
@@ -0,0 +1,7 @@
+from random import choice
+aluno1 = input('Primeiro aluno: ')
+aluno2 = input('Segundo aluno: ')
+aluno3 = input('Terceiro aluno: ')
+aluno4 = input('Quarto aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+print('O aluno escolhido foi {}'.format(choice(lista)))
