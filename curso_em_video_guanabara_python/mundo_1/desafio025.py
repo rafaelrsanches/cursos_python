f6771cc40c6a42563171148e78c50f76cad9d17d
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-08-18 23:56:33 -0300

    Exercício 025 resolvido

--- curso_em_video_guanabara_python/mundo_1/desafio025.py
+++ curso_em_video_guanabara_python/mundo_1/desafio025.py
@@ -1,5 +1,4 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem 'SILVA' no nome.
 
 name = str(input('Digite o nome: ')).strip() # Lê o nome e retira os espaços excedentes
-name_analysis = name.upper().split() # Pôe tudo em maiúsculo e separa as palavras
-print('SILVA' in name_analysis)
+print('SILVA' in name.upper()) # Verifica se tem 'SILVA' na string upper (em letras maiúsculas)
