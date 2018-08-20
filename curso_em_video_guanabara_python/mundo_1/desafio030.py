4fff29757934c1ac6f3cea882cac18be0243cec2
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-08-20 14:36:27 -0300

    Exercício 030 resolvido

---
+++ curso_em_video_guanabara_python/mundo_1/desafio030.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+n = int(input('Digite um número: '))
+print('Este número é PAR.' if n % 2 == 0 else 'Este número é ÍMPAR.')
