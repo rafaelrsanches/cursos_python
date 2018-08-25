cdc82f5d120932ecde9aad3e19c490e01b946857
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-08-25 16:07:35 -0300

    Aula 11 finalizada

---
+++ curso_em_video_guanabara_python/mundo_1/aula11a.py
@@ -0,0 +1,5 @@
+name = 'Rafael Rodrigues Sanches'
+colors = {'clearcolors': '\033[m',
+          'bluesublime': '\033[4;34m',
+          'blackwhite': '\033[7;30m'}
+print('Olá! Seja muito bem-vindo {}{}{}!'.format(colors['bluesublime'], name, colors['clearcolors']))
