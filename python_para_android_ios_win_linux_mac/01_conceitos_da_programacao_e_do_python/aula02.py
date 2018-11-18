8e1b3e6e34df52c7ffd4e2956e965022981938b7
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-17 22:23:06 -0200

    Ponto Flutuante

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/aula02.py
@@ -0,0 +1,17 @@
+#Ponto Flutuante
+
+num_int = 5
+num_dec = 7.3
+val_str = "qualquer texto"
+
+print("O valor é:", num_int)
+print("O valor é: %i" %num_int)
+print("O valor é: " + str(num_int))
+
+print("Concatenando decimal:", num_dec)
+print("Concatenando decimal: %.10f" %num_dec)
+print("Concatenando decimal: " + str(num_dec))
+
+print("Concatenando strings", val_str)
+print("Concatenando strings %s" %val_str)
+print("Concatenando strings " + val_str)
