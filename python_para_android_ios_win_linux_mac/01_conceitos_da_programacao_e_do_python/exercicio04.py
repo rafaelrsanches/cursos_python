186abb5d52935583fd56d1ddd3fd3edd1e1af35d
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-19 21:21:18 -0200

    Exercício 04 resolvido

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/exercicio04.py
@@ -0,0 +1,6 @@
+# Faça um algoritmo que solicite ao usuário informar um número. Em seguida, armazene este número numa variável.
+# Por fim, envie esse número para a saída padrão.
+
+num = input("Digite um número: ")
+
+print(num)
