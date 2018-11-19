a6a972fbea867a8985001ee20b94aeddab2f60fc
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-19 19:49:01 -0200

    Exercício 02 resolvido

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/exercicio02.py
@@ -0,0 +1,6 @@
+# Faça um algoritmo que solicite ao usuário digitar o seu nome e em seguida envie a seguinte frase para a saída
+# padrão: "O seu nome é: [nome do usuário]".
+
+name = input("Digite seu nome: ")
+
+print("O seu nome é:", name)
