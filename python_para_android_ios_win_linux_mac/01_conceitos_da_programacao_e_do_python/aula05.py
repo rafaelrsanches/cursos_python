e6f751fc066b08eadac73bd4da9bab81d17bc878
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-18 22:51:29 -0200

    Módulo da Divisão

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/aula05.py
@@ -0,0 +1,18 @@
+#Módulo da Divisão
+
+# print(3%2)
+# print(4%2)
+# print(5%2)
+# print(7%3.1)
+#
+# print(900%100)
+
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+
+divisao = num1 / num2
+resto = num1 % num2
+
+print()
+print(num1, "dividido por", num2, "é igual a:", divisao)
+print("O resto da divisão entre", num1, "e", num2, "é igual a:", resto)
