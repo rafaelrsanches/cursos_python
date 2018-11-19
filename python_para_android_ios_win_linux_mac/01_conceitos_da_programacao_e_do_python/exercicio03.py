4c674594388eeae90826cb61328f89ea65aa8946
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-19 20:44:27 -0200

    Exercício 03 resolvido

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/exercicio03.py
@@ -0,0 +1,7 @@
+# Faça um algoritmo que solicite o nome e a idade do usuário e então, envie a seguinte frase para o Console:
+# "O seu nome é <nome> e a sua idade é <idade>".
+
+name = input("Digite o seu nome: ")
+age = input("Digite a sua idade: ")
+
+print("O seu nome é", name, "e a sua idade é", age)
