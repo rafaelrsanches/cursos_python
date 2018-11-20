fb8bb63679e9d1544a613b1e543b64cee595f433
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-20 19:09:33 -0200

    Exercício 05 resolvido

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/exercicio05.py
@@ -0,0 +1,6 @@
+# Faça um algoritmo que solicite ao usuário informar um número. Em seguida, escreva a seguinte mensagem:
+# "O número digitado foi: ".
+
+num = input("Digite um número: ")
+
+print("O número digitado foi:", num)
