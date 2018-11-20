dcbc44de458022cb01be8ec2e46193c61ec90f6e
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-20 19:42:02 -0200

    Exercício 06 resolvido

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/exercicio06.py
@@ -0,0 +1,10 @@
+# Faça um algoritmo que solicite ao usuário informar 3 números. Em seguida, some-os e envie para a saída padrão a
+# seguinte frase: "A soma total é: "
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+sum = num1 + num2 + num3
+
+print("A soma total é:", sum)
