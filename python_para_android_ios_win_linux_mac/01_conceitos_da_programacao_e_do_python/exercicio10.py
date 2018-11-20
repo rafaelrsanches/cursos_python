d7647030621016b279970b55b1747b9934c18c5b
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-20 20:46:27 -0200

    Exercício 10 resolvido

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/exercicio10.py
@@ -0,0 +1,10 @@
+# Faça um algoritmo que calcule o cubo e o quadrado de um determinado número:
+
+num = float(input("Digite o número para calcular a potência de 2 e de 3: "))
+
+squared = num ** 2
+cubed = num ** 3
+
+print()
+print("%.2f ao quadrado é %.2f" % (num, squared))
+print("%.2f ao cubo é %.2f" % (num, cubed))
