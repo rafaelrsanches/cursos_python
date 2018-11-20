e19eb9af7f48331f1d55e5eaad1ece2274406bba
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-20 19:49:35 -0200

    Exercício 07 resolvido

---
+++ python_para_android_ios_win_linux_mac/01_conceitos_da_programacao_e_do_python/exercicio07.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo que solicite ao usuário informar 2 números. Em seguida, some os valores e envie para a saída
+# padrão a seguinte frase: "A soma entre <X> e <Y> é igual <total>".
+
+x = float(input("Digite o primeiro número: "))
+y = float(input("Digite o segundo número: "))
+
+total = x + y
+
+print("A soma entre", x, "e", y, "é igual", total)
