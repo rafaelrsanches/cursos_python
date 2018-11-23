f35dfe93f06cb46a4406bc3f1b80007fe0ab551b
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-23 00:03:23 -0200

    Teclas de Atalho à Depuração

---
+++ python_para_android_ios_win_linux_mac/02_pycharm/aula02.py
@@ -0,0 +1,33 @@
+# Teclas de Atalho à Depuração
+
+a = 10
+b = 1, 2, 3
+x = [a, b]
+
+
+def function1():
+    print("qualquer texto")
+
+
+def function2():
+    function1()
+
+
+def function3():
+    function2()
+
+
+def function4():
+    f4 = 50
+    function3()
+
+
+def function5():
+    function4()
+
+
+function5()
+
+print(a)
+print(b)
+print(x)
