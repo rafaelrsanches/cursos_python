6046be99b9cec8eaad2e7aab7e8be7d48008a8a7
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-24 15:29:28 -0200

    Tomada de Decisão II

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/aula01.py
@@ -0,0 +1,11 @@
+# Tomada de Decisão II
+
+action = int(input("Digite '1' para sim e digite '2' para não: "))
+
+if action == 1:
+    print("Você escolheu sim!")
+else:
+    if action == 2:
+        print("Você escolheu não!")
+    else:
+        print("Não existe essa opção!")
