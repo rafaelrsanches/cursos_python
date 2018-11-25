276aa1c0216ff36a720b77f9e12506a903c6aa3b
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-25 14:30:43 -0200

    Blocos de Instrução I

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/aula04.py
@@ -0,0 +1,15 @@
+# Blocos de Instrução I
+
+if True:
+    print("Imprime texto")
+    print("Imprime texto")
+    print("Imprime texto")
+    print("Imprime texto")
+    print("Imprime texto")
+
+if False:
+    print("Não imprime texto")
+    print("Não imprime texto")
+    print("Não imprime texto")
+    print("Não imprime texto")
+    print("Não imprime texto")
