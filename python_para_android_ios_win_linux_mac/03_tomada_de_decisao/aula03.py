c8af7980de118f3451aa548e76654ea0f81668fe
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-25 13:02:30 -0200

    Operadores Relacionais na Prática

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/aula03.py
@@ -0,0 +1,17 @@
+# Operadores Relacionais na Prática
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if num1 == num2:    # operador de igualdade
+    print("O número %i é igual a %i." % (num1, num2))
+if num1 != num2:    # operador diferente de
+    print("O número %i é diferente de %i." % (num1, num2))
+if num1 < num2:     # operador menor que
+    print("O número %i é menor que o %i." % (num1, num2))
+if num1 > num2:     # operador maior que
+    print("O número %i é maior que o %i." % (num1, num2))
+if num1 >= num2:    # operador maior ou igual que
+    print("O número %i é maior ou igual que %i." % (num1, num2))
+if num1 <= num2:    # operador maior ou igual que
+    print("O número %i é menor ou igual que %i." % (num1, num2))
