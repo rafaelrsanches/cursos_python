77a20aa8e62b4fa591b9951e80b1e610c30b1c74
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-25 01:38:44 -0200

    Operadores Relacionais Compostos

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/aula02.py
@@ -0,0 +1,10 @@
+# Operadores Relacionais Compostos
+
+age = int(input("Informe a sua idade: "))
+
+if age <= 0:
+    print("A sua idade não pode ser 0 ou menor do que 0!")
+elif age > 150:
+    print("A sua idade não pode ser superior a 150 anos!")
+elif age < 18:
+    print("Você precisa ter mais do que 18 anos!")
