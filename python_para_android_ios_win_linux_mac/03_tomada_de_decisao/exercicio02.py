3257e3625333216c8dbfefac0f98059a6ef94cb7
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-26 19:32:34 -0200

    Exercício 02 resolvido

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/exercicio02.py
@@ -0,0 +1,8 @@
+# Faça um algoritmo que leia um número e mostre se o mesmo é par ou ímpar.
+
+num = int(input("Digite um número inteiro: "))
+
+if num % 2 == 0:
+    print("O número é par!")
+else:
+    print("O número é ímpar!")
