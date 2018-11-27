310e850690f29647befc9ab27fb7e128f60325cf
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-27 20:42:22 -0200

    Exercício 07 resolvido

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/exercicio07.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo que peça um número ao usuário e verifique se o mesmo é par ou então ímpar.
+# OBS: Erro da lista de exercícios do curso, está igual o exercício 02.
+
+num = int(input("Digite um número inteiro: "))
+
+if num % 2 == 0:
+    print("O número é par!")
+else:
+    print("O número é ímpar!")
