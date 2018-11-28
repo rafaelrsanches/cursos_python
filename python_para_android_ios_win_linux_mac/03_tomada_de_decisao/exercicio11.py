af0cea191486349e21ab774cbbad58e0abd9a13f
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-28 19:53:03 -0200

    Exercício 11 resolvido

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/exercicio11.py
@@ -0,0 +1,8 @@
+# Faça um algoritmo que verifica se um determinado valor é do tipo decimal.
+
+value = 9.98
+
+if type(value) == float:
+    print("A variável é do tipo decimal")
+else:
+    print("A variável não é do tipo decimal")
