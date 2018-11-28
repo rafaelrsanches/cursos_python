685add1b43ae7c9a12a603ffb58dba968ba02025
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-28 19:42:17 -0200

    Exercício 10 resolvido

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/exercicio10.py
@@ -0,0 +1,8 @@
+# Faça um algoritmo que verifica se um determinado valor é uma String.
+
+value = "Hello World!"
+
+if type(value) == str:
+    print("A variável é do tipo String")
+else:
+    print("A variável não é do tipo String")
