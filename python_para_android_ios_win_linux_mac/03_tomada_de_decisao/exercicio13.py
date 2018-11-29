7139e2409b1486767373c3f5d44c8ff168cafdcc
Rafael Rodrigues Sanches <rafael.rsanches96@gmail.com>
2018-11-29 19:09:02 -0200

    Exercício 13 resolvido

---
+++ python_para_android_ios_win_linux_mac/03_tomada_de_decisao/exercicio13.py
@@ -0,0 +1,7 @@
+# Faça um algoritmo que leia três números e imprima na tela o maior deles.
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+print("O maior número é", max(num1, num2, num3))
